1c12894dd920a58afdfa74495fbfb4f7c952a05e
yabu38 <132050455+yabu38@users.noreply.github.com>
2023-09-11 13:06:29 +0000

    Add controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,29 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  scope module: 'public' do
+    root to: "homes#top"
+    get 'homes/about'
+    get "users/current_customer" => "users#show"
+    get "users/confirm"
+    patch "users/withdraw"
+    delete "cart_items/destroy_all"
+    post "orders/confirm"
+    get "orders/thanks"
+    resources :items, only: [:index, :show]
+    resources :users, only: [:edit, :update]
+    resources :addresses, only: [:create, :index, :edit, :update, :destroy]
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    resources :orders, only: [:new, :create, :index, :show]
+  end
+
+  namespace :admin do
+    root to: "homes#top"
+    get "orders/user_show/:id" => "orders#user_show"
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:create, :index, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update, :index]
+  end
 end
