5f4ee644b2a519c0d2a2bf64db2df5b0420660d8
yabu38 <132050455+yabu38@users.noreply.github.com>
2023-09-28 14:15:03 +0000

    Finish admin/items/edit,Add public/items/index,show,items_controller.rb

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -33,8 +33,9 @@ class Admin::ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
+      @item.photos.delete_all
       params[:item][:images].each do |image|
-        @item.photos.update(image: image, item_id: @item.id)
+        @item.photos.create(image: image, item_id: @item.id)
       end
       redirect_to admin_item_path(params[:id])
     else
